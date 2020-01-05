ddb62f68401621340e09dce639ea94f56be4416b
VickyPenkova <vicky.penkova@gmail.com>
2020-01-05 17:58:05 +0200

    Add UserController to be used by the authentication service

---
+++ src/main/java/com/informatics/webservices/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.informatics.webservices.controller;
+
+import com.informatics.webservices.dao.LoginViewModel;
+import com.informatics.webservices.entity.Doctor;
+import com.informatics.webservices.entity.Patient;
+import com.informatics.webservices.service.DoctorService;
+import com.informatics.webservices.service.PatientService;
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+   @Autowired
+   private DoctorService doctorService;
+
+   @Autowired
+   private PatientService patientService;
+
+   //TODO: TEST
+   @RequestMapping(value = "/get/{username}", method = RequestMethod.GET)
+   public Object getUser(@PathVariable("username") String username){
+
+         System.out.println("Searching for user...");
+         Doctor d = doctorService.findDoctorByUsername(username);
+         if (d != null){
+            return d;
+         }
+
+         Patient p = patientService.findPatientByUsername(username);
+         return p;
+   }
+}
